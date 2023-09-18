fc1a0aa94cc198920eb258fbd6f273c964d5be49
AppolinFotso <appolin.s.fotso@gmail.com>
2023-09-18 11:23:09 +0100

    Added react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Videos from "./Videos";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Video Recommendation</h1>
+      </header>
+      <div>
+        <Videos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
